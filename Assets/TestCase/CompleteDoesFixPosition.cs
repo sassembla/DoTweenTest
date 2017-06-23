c83f893a4b329175096b008058bd3f2110cdbfc1
Toru <toru.inoue@kissaki.tv>
2017-06-23 22:24:21 +0900

    add cases.

--- Assets/TestCase/CompleteDoesFixPosition.cs
+++ Assets/TestCase/CompleteDoesFixPosition.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-public class Move : MonoBehaviour {
-
+public class CompleteDoesFixPosition : MonoBehaviour {
 	int frame = 0;
 	// Use this for initialization
 	IEnumerator Start () {
 		var s = DOTween.Sequence();
 		
-		s.Append(transform.DOMove(new Vector3(0,100,0), 100.0f));
+		var endPos = new Vector3(0,100,0);
+		s.Append(transform.DOMove(endPos, 100.0f));
 
 		while (true) {
 			if (frame == 10) {
-				s.Kill();
+				s.Complete();
+				Debug.Assert(transform.position == endPos, "not intended behaviour. position matched.");
+				Debug.Log("after completed pos:" + transform.position);
 				yield break;
 			}
 			yield return null;
---
+++ Assets/TestCase/KillDoesntFixPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class KillDoesntFixPosition : MonoBehaviour {
+
+	int frame = 0;
+	// Use this for initialization
+	IEnumerator Start () {
+		var s = DOTween.Sequence();
+		
+		var endPos = new Vector3(0,100,0);
+		s.Append(transform.DOMove(endPos, 100.0f));
+
+		while (true) {
+			if (frame == 10) {
+				s.Kill();
+				Debug.Assert(transform.position != endPos, "not intended behaviour. position matched.");
+				Debug.Log("after killed pos:" + transform.position);
+				yield break;
+			}
+			yield return null;
+			frame++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
