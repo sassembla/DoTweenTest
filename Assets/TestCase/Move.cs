543a8be67d7d52d9da904cd54c3c75b297890fb8
Toru <toru.inoue@kissaki.tv>
2017-06-23 18:48:46 +0900

    add test case for using DOTween.

---
+++ Assets/TestCase/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	int frame = 0;
+	// Use this for initialization
+	IEnumerator Start () {
+		var s = DOTween.Sequence();
+		
+		s.Append(transform.DOMove(new Vector3(0,100,0), 100.0f));
+
+		while (true) {
+			if (frame == 10) {
+				s.Kill();
+				yield break;
+			}
+			yield return null;
+			frame++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
